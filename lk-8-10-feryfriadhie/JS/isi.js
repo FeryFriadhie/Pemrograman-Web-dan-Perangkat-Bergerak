79bfd8848f2bedd150023042f0b59694381c56f4
Fery Friadhie <82797897+FeryFriadhie@users.noreply.github.com>
2022-04-04 07:56:32 +0700

    Add files via upload

---
+++ lk-8-10-feryfriadhie/JS/isi.js
@@ -0,0 +1,7 @@
+document.writeln("<CENTER>");
+document.writeln("<HR WIDTH=600 COLOR=black>");
+document.writeln("<H1>SMK NEGERI 1 CIAMIS");
+document.writeln("<H2>TEKNIK KOMPUTER DAN INFORMATIKA <H2>");
+document.writeln("<H3>JURUSAN REKAYSA PERANGKAT LUNAK<H3>");
+document.writeln("<HR WIDTH=600 COLOR=black>");
+document.writeln("</CENTER>");
